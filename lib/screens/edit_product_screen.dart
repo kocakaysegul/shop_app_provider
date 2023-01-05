7d191e8e42dfbc34da4b93738092adb299be611b
kocakaysegul <aysegulkocak112@gmail.com>
2023-01-05 14:55:48 +0300

    validation added

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -31,6 +31,13 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   void _updateImageUrl() {
     if (!_imageUrlFocusNode.hasFocus) {
+      if ((!_imageUrlController.text.startsWith('http') &&
+          !_imageUrlController.text.startsWith('https')) ||
+          (!_imageUrlController.text.endsWith('.png') &&
+              !_imageUrlController.text.endsWith('.jpg') &&
+              !_imageUrlController.text.endsWith('.jpeg'))) {
+        return;
+      }
       setState(() {});
     }
   }
@@ -46,6 +53,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
     _form.currentState!.save();
     print(_editedProduct.title);
     print(_editedProduct.description);
@@ -77,6 +88,13 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please provide a value';
+                  } else {
+                    return null;
+                  }
+                },
                 onSaved: (value) {
                   _editedProduct = Product(
                       id: '',
@@ -95,12 +113,25 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please provide a price';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Please enter a valid number.';
+                  }
+                  if (double.parse(value) >= 0) {
+                    return 'Please enter a number greater than zero.';
+                  } else {
+                    return null;
+                  }
+                },
                 onSaved: (value) {
                   _editedProduct = Product(
                       id: '',
                       title: _editedProduct.title,
                       description: _editedProduct.description,
-                      price:  double.parse(value!),
+                      price: double.parse(value!),
                       imageUrl: _editedProduct.imageUrl,
                       isFavorite: false);
                 },
@@ -110,12 +141,22 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please provide a description';
+                  }
+                  if (value.length < 10) {
+                    return 'Should be at least 10 characters long.';
+                  } else {
+                    return null;
+                  }
+                },
                 onSaved: (value) {
                   _editedProduct = Product(
                       id: '',
                       title: _editedProduct.title,
                       description: value.toString(),
-                      price:  _editedProduct.price,
+                      price: _editedProduct.price,
                       imageUrl: _editedProduct.imageUrl,
                       isFavorite: false);
                 },
@@ -147,17 +188,33 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   ),
                   Expanded(
                     child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Image URL'),
-                      keyboardType: TextInputType.url,
-                      textInputAction: TextInputAction.done,
-                      controller: _imageUrlController,
-                      focusNode: _imageUrlFocusNode,
-                      onFieldSubmitted: (_) {
-                        _saveForm();
-                      },
-                      onEditingComplete: () {
-                        setState(() {});
-                      },
+                        decoration: InputDecoration(labelText: 'Image URL'),
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        controller: _imageUrlController,
+                        focusNode: _imageUrlFocusNode,
+
+                        onFieldSubmitted: (_) {
+                          _saveForm();
+                        },
+                        onEditingComplete: () {
+                          setState(() {});
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter an image URL.';
+                          }
+                          if (!value.startsWith('http') &&
+                              !value.startsWith('https')) {
+                            return 'Please enter a valid URL.';
+                          }
+                          if (!value.endsWith('.png') &&
+                              !value.endsWith('.jpg') &&
+                              !value.endsWith('.jpeg')) {
+                            return 'Please enter a valid image URL.';
+                          }
+                          return null;
+                        },
                         onSaved: (value) {
                           _editedProduct = Product(
                               id: '',
@@ -166,8 +223,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
                               price: _editedProduct.price,
                               imageUrl: value.toString(),
                               isFavorite: false);
-                        }
-                    ),
+                        }),
                   ),
                 ],
               ),
